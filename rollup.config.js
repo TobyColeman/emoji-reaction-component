5ae7f192528fa77237fd5ef7a7ae0cfcdc129065
tobycoleman <tobych82@gmail.com>
2019-04-20 12:56:30 +0100

    :package: upgraded to babel 7 (so fun), added emotion, updated lint cfg

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
 import svgr from '@svgr/rollup'
@@ -9,7 +8,7 @@ import svgr from '@svgr/rollup'
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.jsx',
   output: [
     {
       file: pkg.main,
@@ -24,14 +23,10 @@ export default {
   ],
   plugins: [
     external(),
-    postcss({
-      modules: true
-    }),
     url(),
     svgr(),
     babel({
-      exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
+      exclude: 'node_modules/**'
     }),
     resolve(),
     commonjs()
