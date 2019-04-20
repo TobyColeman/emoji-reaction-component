20d78656d934dd527a66ca082f6d853e9f926ae3
tobycoleman <tobych82@gmail.com>
2019-04-20 14:54:24 +0100

    removed cross-compoent (just using an svg instead)

--- stories/cross.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import { storiesOf } from '@storybook/react'
-
-import Cross from '../src/Cross'
-
-storiesOf('Cross', module)
-  .add('default', () => <Cross isOpen={true} />)
