6160ee52e57362cde87191443cc5447d54d12e57
tobycoleman <tobych82@gmail.com>
2019-04-22 00:52:44 +0100

    centered the cta stories to make the anim a bit easier to debug

--- stories/cta.stories.js
+++ stories/cta.stories.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { actions } from '@storybook/addon-actions'
+import centered from '@storybook/addon-centered/react'
 
 import Cta from '../src/Cta'
 
 const ctaActions = actions('onOpen', 'onClose')
 
 storiesOf('Cta', module)
+  .addDecorator(centered)
   .add('When open', () => <Cta {...ctaActions} isOpen={true} />)
   .add('When closed', () => <Cta {...ctaActions} isOpen={false} />)
