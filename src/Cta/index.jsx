51de09e26d8f7003976461e1d4376c17b221d90b
tobycoleman <tobych82@gmail.com>
2019-04-21 16:30:10 +0100

    :sparkles: added a basic rolling animaton for the cross

--- src/Cta/index.jsx
+++ src/Cta/index.jsx
@@ -1,23 +1,50 @@
 import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { css } from '@emotion/core'
+import posed from 'react-pose'
 
 import Cross from '../icons/cross.svg'
 
 import usePrevious from '../hooks/use-previous'
 
+const OPEN_CLOSE_ANIMATION_DURATION = 300 // 300ms
+
+const Box = posed.div({
+  open: {
+    width: '50px',
+    transition: { duration: OPEN_CLOSE_ANIMATION_DURATION }
+  },
+  closed: {
+    width: '16px',
+    transition: { duration: OPEN_CLOSE_ANIMATION_DURATION }
+
+  }
+})
+
+const Box2 = posed.div({
+  open: {
+    x: '34px',
+    y: '0px',
+    rotate: '45deg',
+    transition: { duration: OPEN_CLOSE_ANIMATION_DURATION }
+  },
+  closed: {
+    x: '0px',
+    y: '0px',
+    rotate: '-90deg',
+    transition: { duration: OPEN_CLOSE_ANIMATION_DURATION }
+  }
+})
+
 const styles = css`
   display: inline-flex;
   flex-direction: row;
   background-color: #f7f7f7;
-  border-radius: 50%;
+  border-radius: 8px;
 `
 
 const crossBaseCss = css`
   display: flex;
-`
-const crossOpen = css`
-  transform: rotate(45deg);
 `
 
 export default function Cta(props = { isOpen: false }) {
@@ -30,11 +57,17 @@ export default function Cta(props = { isOpen: false }) {
   })
 
   return (
-    <div css={styles} onClick={() => setOpen(!isOpen)}>
-      <Cross
-        css={[crossBaseCss, !isOpen && crossOpen]}
-      />
-    </div>
+    <Box
+      css={styles}
+      onClick={() => setOpen(!isOpen)}
+      pose={isOpen ? 'open' : 'closed'}
+    >
+      <Box2 pose={isOpen ? 'open' : 'closed'}>
+        <Cross
+          css={[crossBaseCss]}
+        />
+      </Box2>
+    </Box>
   )
 }
 
