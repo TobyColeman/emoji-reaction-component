19f0c051301211d8b767e896897c68b6acb2799e
tobycoleman <tobych82@gmail.com>
2019-04-24 22:37:00 +0100

    :sparkles: added some animations to tie the component together

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,47 @@
+import { useState } from 'react'
 import { css } from '@emotion/core'
+import posed from 'react-pose'
 
-export default function ExampleComponent () {
-  const color = 'white'
+import Cta from './cta'
+import ReactionPopup from './reaction-popup'
+
+const OPEN_CLOSE_ANIMATION_DURATION = 150 // (ms)
+
+const containerStyles = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const reactionPopupStyles = css`
+  z-index: 2;
+`
+
+const TmpWrapper = posed.div({
+  open: {
+    opacity: 1,
+    y: '0px',
+    transition: { duration: OPEN_CLOSE_ANIMATION_DURATION }
+  },
+  closed: {
+    opacity: 0,
+    y: '10px',
+    transition: { duration: OPEN_CLOSE_ANIMATION_DURATION }
+  }
+})
+
+export default function EmojiReaction () {
+  const [isPopupOpen, setPopupOpen] = useState(false)
 
   return (
-    <div
-      css={css`
-      padding: 32px;
-      background-color: hotpink;
-      font-size: 24px;
-      border-radius: 4px;
-      &:hover {
-        color: ${color};
-      }
-    `}
-    >
-    Hover to change color.
+    <div css={containerStyles}>
+      <TmpWrapper css={reactionPopupStyles} pose={isPopupOpen ? 'open' : 'closed'}>
+        <ReactionPopup />
+      </TmpWrapper>
+      <Cta
+        onOpen={() => setPopupOpen(true)}
+        onClose={() => setPopupOpen(false)}
+      />
     </div>
   )
 }
