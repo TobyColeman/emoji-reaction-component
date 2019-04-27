1f1027092259abdb185a453d85991916c2a6dab7
tobycoleman <tobych82@gmail.com>
2019-04-27 12:45:40 +0100

    :bug: fixed reaction popup pushing cta button downward

--- src/index.jsx
+++ src/index.jsx
@@ -8,24 +8,30 @@ import ReactionPopup from './reaction-popup'
 const OPEN_CLOSE_ANIMATION_DURATION = 150 // (ms)
 
 const containerStyles = css`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
 `
 
+const reactionPopupHiddenStyles = css`
+  z-index: -1;
+`
+
 const reactionPopupStyles = css`
+  position: absolute;
   z-index: 2;
 `
 
 const TmpWrapper = posed.div({
   open: {
     opacity: 1,
-    y: '0px',
+    y: '-100%',
     transition: { duration: OPEN_CLOSE_ANIMATION_DURATION }
   },
   closed: {
     opacity: 0,
-    y: '10px',
+    y: '-90%',
     transition: { duration: OPEN_CLOSE_ANIMATION_DURATION }
   }
 })
@@ -35,7 +41,10 @@ export default function EmojiReaction () {
 
   return (
     <div css={containerStyles}>
-      <TmpWrapper css={reactionPopupStyles} pose={isPopupOpen ? 'open' : 'closed'}>
+      <TmpWrapper
+        css={[reactionPopupStyles, !isPopupOpen && reactionPopupHiddenStyles]}
+        pose={isPopupOpen ? 'open' : 'closed'}
+      >
         <ReactionPopup />
       </TmpWrapper>
       <Cta
