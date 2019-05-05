7439010d22b3d517ab769287e40c00a3ee2aa2a0
tobycoleman <tobych82@gmail.com>
2019-05-05 19:20:02 +0100

    updated example

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'emoji-reaction'
+import EmojiReaction, {
+  TitledReaction,
+  AngryReaction,
+  CryReaction,
+  HappyReaction
+} from  'emoji-reaction'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+      <EmojiReaction
+        onReact={(reaction) => { console.log(`Clicked: ${reaction}`) }}
+        reactions={[
+          <TitledReaction title={'Grr'} reaction={AngryReaction} />,
+          <TitledReaction title={'Awesome'} reaction={HappyReaction} />,
+          <TitledReaction title={'oof'} reaction={CryReaction} />
+        ]} />
       </div>
     )
   }
